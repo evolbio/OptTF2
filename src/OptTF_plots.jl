80fba747b762276df5023dda234c7d91bac4df3d
Steve Frank <saf@stevefrank.org>
2022-06-27 10:14:20 -0700

    Rewrote plot_callback() to put all plots on log10 scale.

--- src/OptTF_plots.jl
+++ src/OptTF_plots.jl
@@ -8,6 +8,7 @@ export plot_callback, plot_stoch, plot_temp, plot_stoch_dev_dur, save_summary_pl
 # must have file extension
 file_stem(file) = basename(file[1:findlast(isequal('.'),file)-1])
 file_ext(file) = basename(file[findlast(isequal('.'),file)+1:end])
+log10p(x) = log10(1+x)
 
 function plot_callback(loss_val, S, L, G, pred_all, show_all; no_display=false)
 	lw = 2
@@ -18,18 +19,15 @@ function plot_callback(loss_val, S, L, G, pred_all, show_all; no_display=false)
 	pred = @view pred_all[S.n+1:S.n+S.m,:]
 	
 	log10_yrange = 4
-	log10_switch = log10(S.switch_level)
-	log10_bottom = log10_switch - (log10_yrange / 2)
-	# for example, 1og10 range of 4 and switch at 1e3 yields range (1e1,1e5)
-	yrange = (10^(log10_switch - log10_yrange/2), 10^(log10_switch + log10_yrange/2))
+	p_range = (-0.2,5.2)
+	m_range = (-0.1,2.1)
 	if !show_all
 		# plot current prediction against data, show only target proteins
 		dim = length(pred[:,1])
 		plt = plot(size=(600,400*dim), layout=(dim,1),left_margin=12px)
 		for i in 1:dim
-			plot!(plt, ts, pred[i,:], label = "", color=mma[1], subplot=i,
-					yscale=(i==1 ? :log10 : :identity), ylim=(i==1 ? yrange : :auto),
-					linewidth=lw)
+			plot!(plt, ts, log10p.(pred[i,:]), label = "", color=mma[1], subplot=i,
+					ylim=p_range, linewidth=lw)
 		end
 	else
 		dim = length(pred_all[:,1])
@@ -38,24 +36,24 @@ function plot_callback(loss_val, S, L, G, pred_all, show_all; no_display=false)
 		for i in 1:dim
 			# proteins [S.n+1:2S.n] on left, mRNA [1:S.n] on right
 			idx = vcat(2:2:dim,1:2:dim) 	# subplot index [2,4,6,...,1,3,5,...]
-			plot!(plt, ts, pred_all[i,:], label = "", color=mma[1], subplot=idx[i],
-				yscale=(idx[i]==1 ? :log10 : :identity),
-				ylim=(idx[i]==1 ? yrange : :auto), linewidth=lw)
+			y = log10p.(pred_all[i,:])
+			plot!(plt, ts, y, label = "", color=mma[1], subplot=idx[i],
+				ylim=(isodd(idx[i]) ? p_range : m_range), linewidth=lw)
 		end			
 	end
 	# output normalized by hill vs target normalized by hill
-	target = 10.0.^((log10_yrange-0.1) * hill.(0.5,L.hill_k,G.input_true[1:len])
-						.+ (log10_bottom + 0.05) * ones(len))
-	output = 10.0.^((log10_yrange-0.1) *  hill.(S.switch_level,L.hill_k,pred[1,:])
-						.+ (log10_bottom + 0.05) * ones(len))
+	
+	target = (log10_yrange * hill.(0.5,L.hill_k,G.input_true[1:len])) .+ ones(len)
+	output = (log10_yrange * hill.(S.switch_level,L.hill_k,pred[1,:])) .+ ones(len)
 	plot!(plt, ts, target, label = "", color=mma[2], subplot=1,
-		yscale=:log10, ylim=yrange, linewidth=lw)
+		ylim=p_range, linewidth=lw)
 	plot!(plt, ts, output, label = "", color=mma[3], subplot=1,
-		yscale=:log10, ylim=yrange, linewidth=lw)
+		ylim=p_range, linewidth=lw)
 	
 	# show noisy signal, normalize height to max of associated protein concentration
-	max_conc = maximum(pred[2,:])
-	plot!(ts, max_conc * G.input_noisy[1:len], label = "", color=mma[2],
+	# divide by 2 because signal is on [0,2]
+	max_conc = log10p(maximum(pred[2,:])/2)
+	plot!(ts, (max_conc * G.input_noisy[1:len]), label = "", color=mma[2],
 					subplot=((show_all) ? 3 : 2), linewidth=lw)
 	# add vertical lines for day/night changes, horiz line in plot 1 for expression switch
 	plot!([S.switch_level], seriestype =:hline, color = :black, linestyle =:dot, 
